8a792d7ee3a3035bf62b5f23315592f42a9eaafa
Travis Arnold <ftntravis@gmail.com>
2023-03-23 01:34:12 -0700

    use plugin for config

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,32 +1,10 @@
-import { readFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
+import { createMonacoPlugin } from 'next-monaco'
 
-const themePath = resolve(process.cwd(), 'theme.json')
+const withMonaco = createMonacoPlugin({ theme: 'theme.json' })
 
 /** @type {import('next').NextConfig} */
-export default {
+export default withMonaco({
   experimental: {
     appDir: true,
   },
-  env: {
-    MONACO_THEME: await readFile(themePath, 'utf-8'),
-  },
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'asset/resource',
-    })
-
-    return config
-  },
-}
-// import { createMonacoPlugin } from 'next-monaco'
-
-// const withMonaco = createMonacoPlugin({ theme: 'theme.json' })
-
-// /** @type {import('next').NextConfig} */
-// export default withMonaco({
-//   experimental: {
-//     appDir: true,
-//   },
-// })
+})
