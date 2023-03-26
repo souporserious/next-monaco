1fb5007bc695c871f69ac10451bc0b784fe75f5e
Travis Arnold <ftntravis@gmail.com>
2023-03-26 01:54:28 -0700

    fix ts error

--- next-monaco/editor/index.tsx
+++ next-monaco/editor/index.tsx
@@ -2,9 +2,8 @@ import * as React from 'react'
 import { highlightWithScopes } from '@code-hike/lighter'
 import { Editor as ClientEditor } from '../client'
 import 'server-only'
-import './editor.css'
 
-export async function Editor({ defaultValue }: { defaultValue: string }) {
+async function AsyncEditor({ defaultValue }: { defaultValue: string }) {
   const { lines } = await highlightWithScopes(
     defaultValue,
     'tsx',
@@ -28,4 +27,9 @@ export async function Editor({ defaultValue }: { defaultValue: string }) {
       </ClientEditor>
     </div>
   )
+}
+
+export function Editor({ defaultValue }: { defaultValue: string }) {
+  // @ts-expect-error
+  return <AsyncEditor defaultValue={defaultValue} />
 }
