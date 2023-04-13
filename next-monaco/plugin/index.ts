8052e373ee20ccd5e9cb6023315d414fe320974f
Travis Arnold <ftntravis@gmail.com>
2023-04-13 00:31:02 -0700

    move types to copy-webpack-plugin

--- next-monaco/plugin/index.ts
+++ next-monaco/plugin/index.ts
@@ -1,6 +1,8 @@
 import { NextConfig } from 'next'
-import { resolve } from 'node:path'
-import { readFile } from 'node:fs/promises'
+import { resolve, join } from 'node:path'
+import { tmpdir } from 'node:os'
+import { readFile, writeFile } from 'node:fs/promises'
+import CopyPlugin from 'copy-webpack-plugin'
 import { getTypeDeclarations } from './get-type-declarations'
 
 /** Creates a Next.js plugin that configures Monaco Editor. */
@@ -14,23 +16,43 @@ export function createMonacoPlugin({
   return function withMonaco(nextConfig: NextConfig = {}) {
     const getWebpackConfig = nextConfig.webpack
 
-    nextConfig.webpack = (config, options) => {
-      config.module.rules.push({
-        test: /onig\.wasm$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/wasm/onigasm.wasm',
-        },
-      })
+    return async () => {
+      const typesContents = await Promise.all(
+        types.flatMap(getTypeDeclarations)
+      )
+      const typesFilePath = join(tmpdir(), 'types.json')
 
-      if (typeof getWebpackConfig === 'function') {
-        return getWebpackConfig(config, options)
+      await writeFile(typesFilePath, JSON.stringify(typesContents))
+
+      nextConfig.webpack = (config, options) => {
+        if (options.isServer === false) {
+          config.module.rules.push({
+            test: /onig\.wasm$/,
+            type: 'asset/resource',
+            generator: {
+              filename: 'static/wasm/onigasm.wasm',
+            },
+          })
+
+          config.plugins.push(
+            new CopyPlugin({
+              patterns: [
+                {
+                  from: typesFilePath,
+                  to: resolve(process.cwd(), 'public/types.json'),
+                },
+              ],
+            })
+          )
+        }
+
+        if (typeof getWebpackConfig === 'function') {
+          return getWebpackConfig(config, options)
+        }
+
+        return config
       }
 
-      return config
-    }
-
-    return async () => {
       if (nextConfig.env === undefined) {
         nextConfig.env = {}
       }
@@ -41,13 +63,6 @@ export function createMonacoPlugin({
       )
         // replace single line comments with empty string
         .replace(/\/\/.*/g, '')
-
-      // Load library type declarations
-      const typesContents = await Promise.all(
-        types.flatMap(getTypeDeclarations)
-      )
-
-      nextConfig.env.MONACO_TYPES = JSON.stringify(typesContents.flat())
 
       return {
         transpilePackages: [
